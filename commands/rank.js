2247538bbbd3e2a6da476abb31c5bed94b04349a
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-10-13 20:40:17 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -7,12 +7,15 @@ exports.run = async (client, message, args) => {
   
   let xp = await db.fetch(`xp_${user.id}`);
   if (xp === null) xp = 0;
-  let level = 0;
+  let level = await db.fetch(`level_${user.id}`);
+  if (level === null) level = 0
+  
+  let need = utils.need(level+1);
   
   const embed = new Discord.RichEmbed()
     .setColor([54, 57, 63])
     .setAuthor("RANK: " + user.username, user.avatarURL)
-    .setDescription("LEVEL " + level + "[" + xp + "/100]")
+    .setDescription("LEVEL " + level + "[" + xp + "/" + need + "]")
     .setTimestamp();
   message.channel.send(embed);
   
